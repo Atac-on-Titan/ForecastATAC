b6765ae2dd37f97e0e05f4bb353542df91b23131
jonasbarth <jonas.barth.95@gmail.com>
2023-09-08 16:20:41 +0200

    trendfiltering * explain trend filtering script in README * add more comments

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ForecastATAC
 Working project on forecasting delays in ATAC, i.e., the public transportation services of Rome.
+
+# Trend Filtering
+The `trend_filtering_validation` is a script that 
+
+* downloads the processed gathered live data and static data from an AWS S3 bucket
+* creates a graph of the routes
+* runs convex optimisation
+* stores the results of the optimisation and validation in local storage
+
+## How to run
+```bash
+python trend_filtering_validation.py
+```
--- trend_filtering_validation.py
+++ trend_filtering_validation.py
@@ -43,10 +43,10 @@ if __name__ == "__main__":
     logger.setLevel(logging.INFO)
 
     # create data directory if not exists
-
     Path("data").mkdir(parents=True, exist_ok=True)
     Path("data/static").mkdir(parents=True, exist_ok=True)
 
+    # download the necessary data
     bucket_name = "statistical-learning"
     files_to_download = ["trip_live_final.feather", 'static/trips.txt', 'static/stop_times.txt', 'static/stops.txt', 'static/routes.txt']
     file_paths = ["data/live_data_final.feather", 'data/static/trips.txt', 'data/static/stop_times.txt', 'data/static/stops.txt', 'data/static/routes.txt']
@@ -54,34 +54,44 @@ if __name__ == "__main__":
     for object_key, local_file_path in zip(files_to_download, file_paths):
         s3_download(bucket_name, object_key, local_file_path)
 
+    # check that all necessary files are present
     if not all(map(lambda local_file_path: Path(local_file_path).exists(), file_paths)):
         logger.error("Not all files present, analysis not possible.")
         exit(1)
 
     logger.info("All files present.")
 
+    # load the data
     trip_live = pd.read_feather("data/trip_live_final.feather")
-
     route_stops = build_route_stops('data/static/trips.txt', 'data/static/stop_times.txt', 'data/static/stops.txt',
                                     'data/static/routes.txt')
     init_graph = build_stop_graph('data/static/stops.txt', route_stops)
 
+    # build the signal over the vertices
     test_graph = vertex_signal(trip_live, init_graph, wtr='clear')
 
+    # produce difference operator
     difference = difference_op(test_graph, 2)
     vector_time = np.array([x[1] for x in test_graph.nodes(data='elapsed')])
 
-    vlambda = 0.1  # Choosing the regularization hyperparameter
-    x = cp.Variable(shape=len(vector_time))  # Variable
+    # Choosing the regularization hyperparameter
+    vlambda = 0.1
+
+    # Variable for ...
+    x = cp.Variable(shape=len(vector_time))
+
+    # defining the optimization problem
     obj = cp.Minimize((1 / 2) * cp.sum_squares(vector_time - x)
-                      + vlambda * cp.norm(difference @ x, 1))  # defining the optimization problem
+                      + vlambda * cp.norm(difference @ x, 1))
     prob = cp.Problem(obj)
 
+    # solving the optimisation problem
     prob.solve(solver=cp.CVXOPT, verbose=True)
     logger.info('Solver status: {}'.format(prob.status))
 
     congestion_dict = dict(zip(test_graph.nodes, x.value))
 
+    # validation
     logger.info("Creating train-val split.")
     trip_live['time_pre_datetime'] = pd.to_datetime(trip_live['time_pre_datetime']).dt.date
     val_mask = trip_live['time_pre_datetime'] >= np.datetime64('2023-06-09')
