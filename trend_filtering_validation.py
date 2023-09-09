47cae21da01998c72b688187d156d8e9eb55d3f6
jonasbarth <jonas.barth.95@gmail.com>
2023-09-09 12:14:15 +0200

    trendfiltering * add trend filtering validation call to main script * add log statements * rename test_graph to signal_graph * save metrics in a validation directory

--- trend_filtering_validation.py
+++ trend_filtering_validation.py
@@ -1,4 +1,5 @@
 """Script for running the validation."""
+import json
 import logging.config
 from pathlib import Path
 
@@ -7,7 +8,7 @@ import numpy as np
 import pandas as pd
 import requests
 
-from preprocessing import build_route_stops, build_stop_graph
+from preprocessing import build_route_stops, build_stop_graph, get_start_end_hours
 from trend_filtering import vertex_signal, difference_op, trend_filter_validate
 
 
@@ -68,11 +69,12 @@ if __name__ == "__main__":
     init_graph = build_stop_graph('data/static/stops.txt', route_stops)
 
     # build the signal over the vertices
-    test_graph = vertex_signal(trip_live, init_graph, wtr='clear')
+    logger.info("Building signal graph.")
+    signal_graph = vertex_signal(trip_live, init_graph, wtr='clear')
 
     # produce difference operator
-    difference = difference_op(test_graph, 2)
-    vector_time = np.array([x[1] for x in test_graph.nodes(data='elapsed')])
+    difference = difference_op(signal_graph, 2)
+    vector_time = np.array([x[1] for x in signal_graph.nodes(data='elapsed')])
 
     # Choosing the regularization hyperparameter
     vlambda = 0.1
@@ -85,11 +87,12 @@ if __name__ == "__main__":
                       + vlambda * cp.norm(difference @ x, 1))
     prob = cp.Problem(obj)
 
+    logger.info("Solving optimisation problem.")
     # solving the optimisation problem
     prob.solve(solver=cp.CVXOPT, verbose=True)
     logger.info('Solver status: {}'.format(prob.status))
 
-    congestion_dict = dict(zip(test_graph.nodes, x.value))
+    congestion_dict = dict(zip(signal_graph.nodes, x.value))
 
     # validation
     logger.info("Creating train-val split.")
@@ -97,3 +100,27 @@ if __name__ == "__main__":
     val_mask = trip_live['time_pre_datetime'] >= np.datetime64('2023-06-09')
     train_data = trip_live[~val_mask]
     val_data = trip_live[val_mask]
+
+    logger.info("Creating filters for day, weather, and time.")
+    day_filters = [("day", day) for day in range(0, 7)]
+    weather_filters = [("weather", weather) for weather in ["Clouds", "Clear", "Rain"]]
+
+    start_end_hours = [get_start_end_hours(hour) for hour in range(0, 24)]
+    time_filters = [("time", (start, end)) for start, end in start_end_hours]
+
+    filters = day_filters + weather_filters + time_filters
+
+    lambda_seq = (0.001, 0.01, 0.1)
+
+    validation_dir = "validation"
+    logger.info(f"Creating directory {validation_dir} for validation results.")
+    Path(validation_dir).mkdir(parents=True, exist_ok=True)
+
+    for trend_filter in filters:
+        logger.info(f"Running trend filter validation with filter: {trend_filter} and lambda values: {lambda_seq}")
+        metrics = trend_filter_validate(train_data, val_data, signal_graph, lambda_seq, trend_filter)
+
+        metrics_file = f"{validation_dir}/val_{trend_filter}.json"
+        logger.info(f"Saving validation metrics to {metrics_file}")
+        with open(metrics_file, "w") as outfile:
+            json.dump(metrics, outfile)
